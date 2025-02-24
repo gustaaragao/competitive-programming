9e42b053b5e2c6bc1ecd591dfe67f3c3baa0a56f
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-02-24 16:10:01 -0300

    pitagoras

---
+++ Exercicios/Teoria dos Números/pitagoras.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve(int x, int y, int z) {
+    int x2 = x*x, y2 = y*y, z2 = z*z;
+
+    if (x2 == y2 + z2 || y2 == x2 + z2 || z2 == x2 + y2) {
+        
+    }
+}
+
+int main(){ _
+    int x, y, z;
+    
+    while(cin >> x >> y >> z) {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+        cout << "z = " << z << endl;
+    }    
+
+    cout << "a" << endl;
+
+    return 0;
+}
