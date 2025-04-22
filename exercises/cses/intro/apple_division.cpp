2fccc4b324882fe3ddb79df6ef99c3f65b57804f
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-04-21 22:24:11 -0300

    cses problems

---
+++ exercises/cses/intro/apple_division.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define ff first
+#define ss second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+const ll P = 1000000007;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+
+    int n; cin >> n;
+    vector<ll> p(n); for (int i = 0; i < n; i++) cin >> p[i];         
+
+    return 0;
+}
