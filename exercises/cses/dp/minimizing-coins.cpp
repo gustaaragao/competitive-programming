9db088d80a91c8dc90ce119429ddb4c28e2976dd
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-08-11 09:59:00 -0300

    solving minimizing-coins CSES

--- exercises/cses/dp/minimizing-coins.cpp
+++ exercises/cses/dp/minimizing-coins.cpp
@@ -16,19 +16,31 @@ typedef long long ll;
 typedef pair<int, int> pi;
 
 const ll MOD = 1000000007;
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 #define MAX_N 1000000
 
 ll dp[MAX_N + 1];
+vector<ll> coins;
 
 signed main(){ _
 	
-	int n, x; cin >> n >> x;
-	vector<int> coins(n);
-	for (int i = 0; i < n; i++) cin >> coins[i];
+	ll n, x; cin >> n >> x;
+	for (int i = 0; i < n; i++) {
+		ll c; cin >> c;
+		coins.pb(c);
+	}
+
+	sort(all(coins));
 
 	dp[0] = 0;
+	for (int i = 1; i <= x; i++) {
+		dp[i] = INT_MAX;
+		for (auto c : coins) {
+			if (i - c >= 0) {
+				dp[i] = min(dp[i], dp[i - c] + 1);
+			}
+		}
+	}
 
+	cout << ((dp[x] == INT_MAX) ? -1 : dp[x]) << endl;
 }
