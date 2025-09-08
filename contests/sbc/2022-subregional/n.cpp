a7951de0a2a4210235a0e8f54e959bdfc5c1c9dc
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-09-08 12:15:25 -0300

    subregional 2022

--- contests/sbc/2022-subregional/n.cpp
+++ contests/sbc/2022-subregional/n.cpp
@@ -17,11 +17,6 @@ typedef pair<int, int> pi;
 
 const ll MOD = 1e9 + 7;
 
-// Faz a soma de a[L...R], sendo que a está 1-indexed
-int qry(int l, int r, const vector<int> &sum) {
-    return sum[r] - sum[l-1];
-}
-
 signed main(){
     FAST_IO
 
@@ -30,15 +25,61 @@ signed main(){
     for (auto &x: a) cin >> x;
     for (auto &x: b) cin >> x;
     int k, l; cin >> k >> l;
-    // Calcular a soma acumulada para a e b
-    vector<int> sum_a(n+1), sum_b(n+1); // 1-indexed
-    sum_a[1] = a[0]; sum_b[1] = b[0];
-    for (int i = 2; i <= n; ++i) {
-        sum_a[i] = sum_a[i-1] + a[i-1];
-        sum_b[i] = sum_b[i-1] + b[i-1];
-    }
-    ll ans = 0;
-    // Brutar todas as possibilidades para K
+    // sum_k -> soma dos K valores
+    // sum_l -> soma dos L valores (Soma ótima)
+    ll sum_k = 0, sum_l = 0;
+    // s -> L maiores valores do conjunto K | t -> (K - L) valores restantes
+    multiset<int> s, t;
+    // A função relax() serve para manter |s| = L e de |t| = K - L
+    auto relax = [&] () {
+        while (s.size() > l) {
+            // Conjunto s tem tamanho maior que o máximo L
+            auto it = s.begin(); // remova o menor valor do conjunto s
+            sum_l -= *it;        // atualize a soma
+            t.insert(*it);       // adicione esse valor no conjunto t
+            s.erase(it);         // remove o valor s
+        }
+        while (s.size() < l && !t.empty()) {
+            // Conjunto s tem tamanho menor que o L e Conjunto t não está vazio
+            auto it = t.end(); // remova o maior valor do conjunto t
+            it--;
+            sum_l += *it;        // atualize a soma
+            s.insert(*it);       // adicione esse valor no conjunto s
+            t.erase(it);         // remove o valor t
+        }
+    };
+    // Insere no conjunto s
+    auto insert = [&] (int i) {
+        sum_l += b[i];
+        s.insert(b[i]);
+        relax();
+    };
+    // Remove do conjunto s ou t
+    auto remove = [&] (int i) {
+        if (s.count(b[i])) {
+            s.erase(b[i]);
+            sum_l -= b[i];
+        } else {
+            t.erase(b[i]);
+        }
+        relax();
+    };
     
+    // Primeiro Caso: Prefixo = K e Sufixo = 0
+    for (int i = 0; i < k; i++) {
+        sum_k += a[i];
+        insert(i);
+    }
+    ll ans = sum_k + sum_l;
+    for (int i = k - 1, j = 1; i >= 0; i--, j++) {
+        // Remove a carta i
+        sum_k -= a[i];
+        remove(i);
+        // Adiciona a carta n - j
+        sum_k += a[n-j];
+        insert(n - j);
+
+        ans = max(ans, sum_k + sum_l);
+    } 
     cout << ans << endl;
-}   
+}
