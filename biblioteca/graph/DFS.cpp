8cdb28ef69ceee01277850658241da65a1fdcaa0
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-10-05 18:22:09 -0300

    vim

--- biblioteca/graph/DFS.cpp
+++ biblioteca/graph/DFS.cpp
@@ -1,29 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std; 
-
 #define pb push_back
 #define MAXN 100005
-
-// adj -> lista de adjacências
 vector<int> adj[MAXN];
-// visited[i] -> marca que o vértice i foi visitado
 bool visited[MAXN];
-// n -> qtd de vértices, m -> qtd de arestas
 int n, m;
-
 // O(v + E)
-// o caminho encontrado do DFS é o PRIMERIO LEXICOGRAFICAMENTE em um grafo
-// Em uma árvore, encontra o menor caminho entre u e v
-void dfs(int s) { // s -> vértice inicial
-    for (int i = 0; i < adj[s].size(); i++) {
-        int v = adj[s][i];
-        if (!visited[v]) {
-            visited[v] = visited[i];
-            dfs(v);
+void dfs(int v) {
+    visited[v] = true;
+    for (auto u : adj[v]) {
+        if (!visited[u]) {
+            dfs(u);
         }
     }
 }
-
 signed main() {
     cin >> n >> m;
     for (int i = 0; i < m; i++) {
@@ -34,4 +24,3 @@ signed main() {
     }
     dfs(0);
 }
-// https://cp-algorithms.com/graph/breadth-first-search.html
