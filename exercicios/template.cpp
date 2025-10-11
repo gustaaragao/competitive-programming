773ed665b2c8bf4424bf0c41128c9e0ee2217801
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-10-10 23:05:25 -0300

    organizando o repo

---
+++ exercicios/template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+
+#define endl '\n'
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define dbg(x) cout << #x << " = " << x << endl
+
+template <class T>
+using ord_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+	
+}
+
