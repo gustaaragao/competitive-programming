69a0527f9367af553499feda87a60fdf68d21791
gustaaragao <gustavohenriquearagaosilva@gmail.com>
2025-12-17 10:00:29 -0300

    DSU

--- biblioteca/structures/DSU.cpp
+++ biblioteca/structures/DSU.cpp
@@ -1,2 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct dsu {
+    // pai = representante
+    vector<int> parent, size;
+    dsu(int n) {
+        parent.resize(n);
+        size.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int find(int i) { 
+        // Path Compression
+        return parent[i] = (parent[i] == i) ? i : find(parent[i]);
+    }
+    void uni(int a, int b) { // o pai de a se torna pai de b
+        a = find(a), b = find(b);
+        if (a != b) {
+            // a -> maior árvore
+            if (size[a] < size[b]) { // Small to Large Otimization
+                swap(a, b);
+            }
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+};
+// https://cp-algorithms.com/data_structures/disjoint_set_union.html
--- contests/advent-of-code-2025/day08.cpp
+++ contests/advent-of-code-2025/day08.cpp
@@ -4,6 +4,33 @@ using namespace std;
 #define endl '\n'
 #define int long long int
 
+struct dsu {
+    vector<int> parent, size;
+    dsu(int n) {
+        parent.resize(n);
+        size.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int find(int i) {
+        return parent[i] = (parent[i] == i) ? i : find(parent[i]);
+        // return (parent[i] == i) ? i : find(parent[i]);
+    }
+    void uni(int a, int b) { // a se torna pai de b
+        a = find(a), b = find(b);
+        if (a != b) {
+            // a -> maior árvore
+            if (size[a] < size[b]) {
+                swap(a, b);
+            }
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+};
+
 vector<string> tokenizer(string s, string del = " ") {
     // Time: O(n) Space: O(1) 
     vector<string> tokens;
@@ -30,7 +57,46 @@ signed main() {
             stoll(tokens[2])
         );
     }
-    for (auto [x, y, z] : arr) {
-        cout << x << " " << y << " " << z << endl;
+    vector<tuple<int, int, int>> edges;
+    int n = (int) arr.size();
+    for (int i = 0; i < n; i++) {
+        auto [x1, y1, z1] = arr[i];
+        for (int j = i+1; j < n; j++) {
+            auto [x2, y2, z2] = arr[j];
+            int dx = x1 - x2, dy = y1 - y2, dz = z1 - z2;
+            int dist = dx*dx + dy*dy + dz*dz;
+            edges.push_back({dist, i, j});
+        }
     }
+    sort(edges.begin(), edges.end());
+    // Union Find
+    dsu d(n); 
+    int t = 0;
+    // for (auto [dist, i, j] : edges) {
+    //     if (d.find(i) != d.find(j)) {
+    //         d.uni(i, j);
+    //     }
+    //     t++;
+    //     if (t == 10) break;
+    // }
+    int last_i = -1, last_j = -1;
+    for (auto [dist, i, j] : edges) {
+        if (d.find(i) != d.find(j)) {
+            d.uni(i, j);
+            last_i = i;
+            last_j = j;
+        }
+        if (d.size[d.find(i)] == n) {
+            break;
+        }
+    }
+    auto [x1, y1, z1] = arr[last_i];
+    auto [x2, y2, z2] = arr[last_j];
+    cout << x1 * x2 << endl;
+    // vector<int> cnt(n, 0); // cnt[pai] = qtd de elementos do conjunto de pai
+    // for (int i = 0; i < n; i++) {
+    //     cnt[d.find(i)]++;
+    // }
+    // sort(cnt.rbegin(), cnt.rend());
+    // cout << cnt[0] * cnt[1] * cnt[2] << endl;
 }
